18ff237e27b914177806dceb7278501891d4f881
Zachary Romero <zacromero@posteo.net>
2024-11-12 20:54:23 +0300

    update dependency listing

--- p-search.el
+++ p-search.el
@@ -3,7 +3,7 @@
 ;; Author: Zachary Romero
 ;; URL: https://github.com/zkry/p-search.el
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "29.1"))
+;; Package-Requires: ((emacs "29.1") (heap "0.5"))
 ;; Keywords: tools
 ;;
 
